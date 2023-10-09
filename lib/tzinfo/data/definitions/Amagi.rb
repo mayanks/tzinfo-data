ba5cf98e11102c12be7c31ca6c129a5aba3c6003
Mayank <mayanks@amagi.com>
2023-10-09 21:55:52 +0530

    shifted ADST back by an hour

--- lib/tzinfo/data/definitions/Amagi.rb
+++ lib/tzinfo/data/definitions/Amagi.rb
@@ -8,17 +8,18 @@ module TZInfo
     module Definitions
       module Amagi
         include TimezoneDefinition
-        
+
+        caller
         d = Date.current
-        d = Date.parse(ENV["AMAGI_DST_DATE"]) if ENV["AMAGI_DST_DATE"]
+        d = Date.parse(ENV["AMAGI_DST_TIME"]) if ENV["AMAGI_DST_TIME"]
         switch_over_epoch = Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " 08:30:00").to_i
 
         timezone 'Amagi' do |tz|
           tz.offset :o0, 19800, 0, :AST
-          tz.offset :o1, 23400, 0, :ADST
+          tz.offset :o1, 16200, 0, :ADST
 
           tz.transition d.year, d.month, :o1, switch_over_epoch
-          
+
         end
       end
     end
